f8d841dfd9498db10aa6f65a3269e1686b9b3f7b
AbhiC7721 <abhic7721@gmail.com>
2021-02-02 16:28:14 +0530

    Used my own generated id

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,19 @@
 // CRUD -- create read update and delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+
+//object destructuring (shorthand for the above code)
+const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
     if(error) {
@@ -14,6 +23,7 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
     const db = client.db(databaseName)
 
     db.collection('users').insertOne({
+        _id: id,
         name: 'Abhinaba',
         age: 20
     }, (error, result) => {
@@ -23,4 +33,39 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
         console.log(result.ops)
     })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Rajdeep', 
+    //         age: 21
+    //     }, {
+    //         name: 'Sayantan',
+    //         age: 20
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Clean the house',
+    //         completed: true
+    //     }, {
+    //         description: 'Renew inspection', 
+    //         completed: false
+    //     }, {
+    //         description: 'Pot plants', 
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return cosole.log('Unable to insert tasks!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
 })
