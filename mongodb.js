d39976af1da74f491910bfddba41bd76730c8d8e
AbhiC7721 <abhic7721@gmail.com>
2021-02-06 22:18:35 +0530

    Length of string id

--- mongodb.js
+++ mongodb.js
@@ -12,8 +12,9 @@ const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
+console.log(id.id.length)
+// console.log(id.getTimestamp())
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
     if(error) {
@@ -22,17 +23,17 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Abhinaba',
-        age: 20
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user')
-        }
+    // db.collection('users').insertOne({
+    //     _id: id,
+    //     name: 'Abhinaba',
+    //     age: 20
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
 
-        console.log(result.ops)
-    })
+    //     console.log(result.ops)
+    // })
 
     // db.collection('users').insertMany([
     //     {
