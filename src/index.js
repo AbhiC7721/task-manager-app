7ddf0996edb6d5a7cba712fcfee07ee5aad5ca1a
AbhiC7721 <abhic7721@gmail.com>
2021-02-16 17:28:51 +0530

    Added update endpoint for users database

--- src/index.js
+++ src/index.js
@@ -110,28 +110,29 @@ app.get('/tasks/:id', async (req,res) => {
 
 })
 
-// app.patch('/tasks/:id', async(req,res) => {
-//     const updates =  Object.keys(req.body)
-//     const allowedUpdates = ['description', 'completed']
-//     const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
+app.patch('/tasks/:id', async(req,res) => {
+    const updates =  Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
 
-//     if(!isValidOperation) {
-//         return res.status(400).send({error: 'Invalid updates!'})
-//     }
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
 
-//     try {
-//         const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
-//         if(!task) {
-//             return res.status(404).send()
-//         }
+        if(!task) {
+            return res.status(404).send()
+        }
 
-//         res.send(task)
-//     } catch(e) {
-//         return res.status(400).send(e)
-//     }
+        res.send(task)
+    } catch(e) {
+        return res.status(400).send(e)
+    }
 
-// })
+})
+
 
 app.listen(port, ()=> {
     console.log('Server is up on port ' + port)    
