2ef8bfa24a2a201b3148150ade062f962810f699
AbhiC7721 <abhic7721@gmail.com>
2021-02-23 02:07:10 +0530

    Added delete option to the user database

--- src/routers/user.js
+++ src/routers/user.js
@@ -91,7 +91,6 @@ router.delete('/users/me', auth, async (req, res) => {
 
 //uploading a profile picture
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -104,10 +103,18 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res)=> {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res)=> {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async(req, res)=> {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 module.exports = router
