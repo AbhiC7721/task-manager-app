ca77a686c32ccb75f9e5e27a939e016eb87b4fac
AbhiC7721 <abhic7721@gmail.com>
2021-02-22 23:21:26 +0530

    File uploads initial

--- src/index.js
+++ src/index.js
@@ -6,18 +6,17 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next)=> {
-//     if(req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
 
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
 
-// app.use((req, res, next)=> {
-//     res.status(503).send('The site is under maintenance. Please try back soon')
-// })
+app.post('/upload', upload.single('upload'),  (req, res) => {
+    res.send()
+})
+
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -27,18 +26,3 @@ app.listen(port, ()=> {
     console.log('Server is up on port ' + port)    
 })
 
-// const Task =  require('./models/task')
-// const User = require('./models/user')
-
-// const main =  async () =>{
-//     // const task =  await Task.findById('6031e09893affd9cdf274c6d')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('603173270248898a64cb1859')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-
-// }
-
-// main()
